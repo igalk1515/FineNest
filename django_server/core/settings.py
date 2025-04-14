32d5c96db3336392e986c07cf5f326e9916e9272
igal <igalk1515@gmail.com>
2025-04-14 11:06:43 -0700

    add cross-origin resource sharing (CORS) support to Django server

--- django_server/core/settings.py
+++ django_server/core/settings.py
@@ -39,9 +39,11 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'receipt',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -132,3 +134,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+
+CORS_ALLOW_ALL_ORIGINS = True
